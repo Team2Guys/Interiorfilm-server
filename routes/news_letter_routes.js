cca43c72dc79aeeb43686a49418199e1ceda652f
MFaad1 <faadsardar123@gmail.com>
2024-10-19 10:53:57 +0500

    fixes

--- routes/news_letter_routes.js
+++ routes/news_letter_routes.js
@@ -3,9 +3,12 @@ const { Router } = require('express');
 const {authenticate} = require('../middleware/authhandler')
 const new_letter_router = Router();
 
-
 new_letter_router.post('/Add_email', controller.promotional_handler)
 new_letter_router.post('/send_promotional_email',authenticate, controller.promotional_forward_handler)
+new_letter_router.get('/get-all',authenticate, controller.get_all_user)
+
+new_letter_router.delete('/del-user/:id',authenticate, controller.deleteProduct)
+
 
 
 
