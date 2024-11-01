7baf28ecc64f6940f7b93c5384cebdcf5041b02b
Mujtaba Shafique <mujtaba.shafique@outlook.com>
2024-11-01 14:39:14 +0500

    fixes

--- controller/sales.js
+++ controller/sales.js
@@ -290,7 +290,6 @@ exports.proceedPayment = async (req, res) => {
     const { productItems, totalAmount, subtotalAmount, ...billing_data } = data;
     const order_id = generateUniqueString();
 
-    console.log('shipmentFee', shipmentFee);
     let sale = await Sale.findOne({ usermail: billing_data.email });
 
     const parsedDate = new Date()
@@ -321,7 +320,7 @@ exports.proceedPayment = async (req, res) => {
     myHeaders.append("Content-Type", "application/json");
     const staticProduct = {
       name: 'Shipping Fee',
-      amount: shipmentFee === 'Free' ? 0 : shipmentFee * 100,
+      amount: shipmentFee === 'Free' || shipmentFee === 'undefine' ? 0 : shipmentFee * 100,
     };
     const products = productItems
       .map(product => ({
